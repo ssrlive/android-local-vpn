149a8dd5520bc0dcb37b1cd6f9717c6329431952
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-10-26 19:31:42 +0800

    reading code

--- source/core/src/vpn/mod.rs
+++ source/core/src/vpn/mod.rs
@@ -7,12 +7,9 @@ mod smoltcp_socket;
 mod utils;
 mod vpn_device;
 
-use mio::Waker;
-use processor::Processor;
-
 pub(super) struct Vpn {
     file_descriptor: i32,
-    stop_waker: Option<Waker>,
+    stop_waker: Option<::mio::Waker>,
     thread_join_handle: Option<std::thread::JoinHandle<()>>,
 }
 
@@ -26,7 +23,7 @@ impl Vpn {
     }
 
     pub fn start(&mut self) {
-        let mut processor = Processor::new(self.file_descriptor);
+        let mut processor = processor::Processor::new(self.file_descriptor);
         self.stop_waker = Some(processor.new_stop_waker());
         self.thread_join_handle = Some(std::thread::spawn(move || processor.run()));
     }
