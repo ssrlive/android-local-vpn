99f028dd99e8c417bbd2f0ac3d402ff9f82a1192
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-10-26 19:19:22 +0800

    reading code

--- source/core/src/vpn/buffers.rs
+++ source/core/src/vpn/buffers.rs
@@ -13,26 +13,20 @@ impl Buffers {
         }
     }
 
-    pub(crate) fn consume_data<F>(&mut self, direction: OutgoingDirection, mut write_fn: F)
+    pub(crate) fn consume_data<F>(&mut self, direction: OutgoingDirection, mut consume_fn: F)
     where
         F: FnMut(&[u8]) -> crate::Result<usize>,
     {
         match self {
             Buffers::Tcp(tcp_buf) => {
                 let buffer = tcp_buf.peek_data(&direction).to_vec();
-                match write_fn(&buffer[..]) {
+                match consume_fn(&buffer[..]) {
                     Ok(consumed) => {
                         tcp_buf.consume_data(&direction, consumed);
                     }
                     Err(error) => match error {
-                        crate::Error::Io(err) => {
-                            if err.kind() == ErrorKind::WouldBlock {
-                            } else {
-                                log::error!("failed to write tcp, direction: {:?}, error={:?}", direction, err);
-                            }
-                        }
-                        crate::Error::TcpSend(err) => {
-                            log::error!("failed to write tcp, direction: {:?}, error={:?}", direction, err);
+                        crate::Error::Io(error) if error.kind() == ErrorKind::WouldBlock => {
+                            log::trace!("failed to write tcp, direction: {:?}, error={:?}", direction, error);
                         }
                         _ => {
                             log::error!("failed to write tcp, direction: {:?}, error={:?}", direction, error);
@@ -45,22 +39,19 @@ impl Buffers {
                 let mut consumed: usize = 0;
                 // write udp packets one by one
                 for datagram in all_datagrams {
-                    if let Err(error) = write_fn(&datagram[..]) {
+                    if let Err(error) = consume_fn(&datagram[..]) {
                         match error {
-                            crate::Error::Io(err) => {
-                                if err.kind() == ErrorKind::WouldBlock {
+                            crate::Error::Io(error) => {
+                                if error.kind() == ErrorKind::WouldBlock {
+                                    log::trace!("failed to write udp, direction: {:?}, error={:?}", direction, error);
                                     break;
                                 } else {
-                                    log::error!("failed to write udp, direction: {:?}, error={:?}", direction, err);
+                                    log::error!("failed to write udp, direction: {:?}, error={:?}", direction, error);
                                 }
                             }
-                            crate::Error::UdpSend(err) => {
-                                use ::smoltcp::socket::udp::SendError;
-                                if err == SendError::BufferFull || err == SendError::Unaddressable {
-                                    break;
-                                } else {
-                                    log::error!("failed to write udp, direciton: {:?}, error={:?}", direction, err);
-                                }
+                            crate::Error::UdpSend(error) => {
+                                log::trace!("failed to write udp, direction: {:?}, error={:?}", direction, error);
+                                break;
                             }
                             _ => {
                                 log::error!("failed to write udp, direction: {:?}, error={:?}", direction, error);
