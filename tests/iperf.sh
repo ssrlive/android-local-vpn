d2b0885da1b472cd278795da73c35f863831b3c4
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-11-05 11:58:01 +0800

    tests

---
+++ tests/iperf.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# sudo apt install -y iperf3 dante-server
+sudo systemctl stop danted
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+netns="test"
+dante="danted"
+testapp="${SCRIPT_DIR}/../target/release/main"
+
+ip netns add "$netns"
+
+ip link add veth0 type veth peer name veth0 netns "$netns"
+
+# Configure veth0 in default ns
+ip addr add 10.0.0.2/24 dev veth0
+ip link set dev veth0 up
+
+# Configure veth0 in child ns
+ip netns exec "$netns" ip addr add 10.0.0.3/24 dev veth0
+ip netns exec "$netns" ip addr add 10.0.0.4/24 dev veth0
+ip netns exec "$netns" ip link set dev veth0 up
+
+# Configure lo interface in child ns
+ip netns exec "$netns" ip addr add 127.0.0.1/8 dev lo
+ip netns exec "$netns" ip link set dev lo up
+
+echo "Starting Dante in background ..."
+ip netns exec "$netns" "$dante" -f ${SCRIPT_DIR}/dante.conf &
+
+# Start iperf3 server in netns
+ip netns exec "$netns" iperf3 -s -B 10.0.0.4 &
+
+sleep 1
+
+# Prepare testapp
+ip tuntap add name tun0 mode tun
+ip link set tun0 up
+ip route add 10.0.0.4 dev tun0
+
+# "${testapp}" --tun tun0 --out veth0 &
+
+# Run iperf client through testapp
+# iperf3 -c 10.0.0.4
+
+# iperf3 -c 10.0.0.4 -R
+
